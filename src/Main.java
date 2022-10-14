4edaec045029da397104bce37e2261b0de9d5b08
Dogggy9 <dogggy9@gmail.com>
2022-10-14 14:35:05 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
-public class Main {
-    public static int a = 10;
-    public int b = 11;
-    public static void some(){
-        int c = 12;
-    }
-    public void test(){
-        int d = 13;
-        {
-            doSomething1();
-            int e = 14;
-            doSomething2();
-            doSomething3();
-        }
+class Main{
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        float c = (float) a / b;
+        System.out.println(c);
     }
 }
